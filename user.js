28de168d7902b0c33ebf281ade96a9a1147b991a
pbbqdd <pbbqdd@gmail.com>
2019-12-07 21:06:32 +0800

    None

--- user.js
+++ user.js
@@ -2,19 +2,18 @@
 // @name         追新番磁力下载按钮
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
+// @description  追新番磁力下载按钮修复
 // @author       You
 // @match        http://www.zhuixinfan.com/main.php?mod=viewresource&sid=*
 // @grant        none
 // ==/UserScript==
 
 (function() {
-    'use strict';
+
     var torrent = document.getElementById("torrent_url");
     var text =torrent.innerHTML;
     var ci = document.getElementsByClassName("bt bt-cl");
-//    ci.parentNode.removeChild(ci);
     ci[0].href = text;
 
-    // Your code here...
 })();
+
